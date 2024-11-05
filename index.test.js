53a4be2ea71d1aca20274827e4a36d8689683700
Tom Evans <74tevans@gmail.com>
2024-11-05 09:34:20 +0000

    app.js working, tests aren't

--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@
 const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
+const {describe, it, expect} = require('@jest/globals')
 
 const request = require("supertest")
 const { db } = require('./db/connection');
@@ -12,7 +13,12 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
+    it('has a working endpoint', async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+        expect(responseData).toEqual(seedMusician);
+    })
     
 
 
